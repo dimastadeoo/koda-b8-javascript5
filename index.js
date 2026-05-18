024b44aa16f4438a1ee98d189532b4fe1c0159c0
dimastadeoo <53636907+dimastadeoo@users.noreply.github.com>
2026-05-18 10:20:28 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+const bil = [4,5,6,3,15,77,100, 17, 7]
+//proses untuk rumus cari bilangan prima
+function isPrime(num) {
+  if (num <= 1) return false
+  if (num === 2) return true
+  if (num % 2 === 0) return false
+  for (let i = 3; i * i <= num; i += 2) {
+    if (num % i === 0) return false;
+  }
+  return true
+}
+const prima = bil.filter(isPrime)
+
+console.log(prima)
